a3ef75e6bf3f9e1ca3ffe58dc595286c27b7b91e
yesobo <jesus.espejo.arnas@gmail.com>
2017-01-13 01:56:43 +0100

    feat(scss): scss compiled to global css file

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import angular from 'angular';
 
 import {AppModule} from './app/app.module';
 
+require('./app/app.scss');
+
 angular.bootstrap(document, [AppModule], {strictDi: true});
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 var path = require('path');
 var webpack = require('webpack');
 
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+let config = {
+  output: {
+    path: __dirname + '/dist'
+  }
+}
+let extractSCSS = new ExtractTextPlugin('bundle.css');
+
 module.exports = {
   entry: './src/main.js',
   output: { 
-    path: __dirname + '/dist', 
-    filename: 'bundle.js' },
+    path: config.output.path, 
+    filename: 'bundle.js' 
+  },
   module: {
     rules: [{
       test: /.js?$/,
@@ -15,8 +25,22 @@ module.exports = {
           presets: ['es2015']
         }
       }]
+    }, {
+      test: /\.s?css$/,
+      loader: extractSCSS.extract({
+          fallbackLoader: 'style-loader',
+          loader: [{
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }
+          ]
+        }),
     }]
-  },
+  },
+  plugins: [
+    extractSCSS
+  ],
   devServer: {
     contentBase: './src'
   }
